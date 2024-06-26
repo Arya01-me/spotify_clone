4edefb590df419d4563c52e87f3a42ea82bd6901
Arya Bhat <136065857+Arya01-me@users.noreply.github.com>
2024-06-26 22:05:02 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
     playlists: [],
     playing: false,
     item: null,
-    token: 'BQDJZYno1pUh3-PFoLKf19k0hpgdlEvBE3IrhXIT-EX6zob-qWZg_ibgKiNONrud335oUJzs9pZnhcP_i-_FQozHu2XrTcnOFIGFjgn1E7FnCqSdZcr_Jcf6_X3I8NDbTjLJLBsIH6DGio6INEr9NrjlOrWZ4CEPkvMj1xuNeR_6WdJL--1fYmenZjSkhlwVz2IDJ-3DCH0ukG684RWh',
+    //token: 'BQDJZYno1pUh3-PFoLKf19k0hpgdlEvBE3IrhXIT-EX6zob-qWZg_ibgKiNONrud335oUJzs9pZnhcP_i-_FQozHu2XrTcnOFIGFjgn1E7FnCqSdZcr_Jcf6_X3I8NDbTjLJLBsIH6DGio6INEr9NrjlOrWZ4CEPkvMj1xuNeR_6WdJL--1fYmenZjSkhlwVz2IDJ-3DCH0ukG684RWh',
 
 };
 const reducer = (state, action) => {
